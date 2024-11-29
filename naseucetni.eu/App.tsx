ecd3d1e55b49156ff1bce2f65e898218c9384e5b
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-11-29 11:15:36 +0100

    app + navigation

--- naseucetni.eu/App.tsx
+++ naseucetni.eu/App.tsx
@@ -1,12 +1,53 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomePage from './screens/HomePage';
+import SettingsScreen from './screens/SettingScreen';
+import PhotoScreen from './screens/PhotoScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarStyle: {
+              backgroundColor: '#060663',
+            },
+            tabBarIcon: ({ color }) => {
+              let iconName: string;
+
+              switch (route.name) {
+                case 'Doklady':
+                  iconName = 'home';
+                  break;
+                case 'Scanner':
+                  iconName = 'plus';
+                  break;
+                case 'Nápověda':
+                  iconName = 'help';
+                  break;
+                default:
+                  iconName = 'alert-circle'; // Fallback icon
+              }
+
+              return <MaterialCommunityIcons name={iconName as keyof typeof MaterialCommunityIcons.glyphMap} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Doklady" component={HomePage} />
+          <Tab.Screen name="Scanner" component={PhotoScreen} />
+          <Tab.Screen name="Nápověda" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
